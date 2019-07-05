d459e703bcfe0d872feab700bdc7f24660557447
jjsayleraxio <josephs@axioresearch.com>
2019-07-05 16:39:34 -0700

    add custom package installers
    these scripts install select bioconductor packages as well as R packages
    developed by Axio Research to the image for use in the container

---
+++ installers/BioC_installr.R
@@ -0,0 +1,55 @@
+#Set so that an external arguments to this script are the list of views to use to identify bioconductor libraries to keep
+tags <- commandArgs(trailingOnly = TRUE)
+print(tags)
+
+#install most recent version of Bioconductor
+
+url <- "http://www.bioconductor.org/packages/release/bioc"
+
+if ("BiocInstaller" %in% rownames(installed.packages()))
+  remove.packages("BiocInstaller")
+
+install.packages("BiocInstaller", repos=url)
+
+#always install these packages
+builtins <- c("Matrix", "KernSmooth", "mgcv")
+
+
+t <- tempfile()
+download.file(paste0(url,"/VIEWS"), t)
+dcf <- as.data.frame(read.dcf(t), stringsAsFactors=FALSE)
+
+BioC_Packages<-dcf$Package
+
+FindPackageWithTag<-function(PackageName,PackageDF,Tag){
+  PackageTags<-gsub("\n","",PackageDF[which(PackageDF$Package==PackageName),"biocViews"])
+  if(!is.na(PackageTags)){
+    tagsList<-strsplit(PackageTags,split = ", ")[[1]]
+    if(any(tagsList%in%Tag)){
+      return(PackageName)
+    }
+  }
+}
+
+bioCpackages<-c(do.call('rbind',lapply(BioC_Packages,FindPackageWithTag,dcf,tags)))
+
+ap.db <- available.packages(contrib.url(BiocInstaller::biocinstallRepos()))
+ap <- rownames(ap.db)
+
+#filter for only available packages and union with builtins
+bioCpackages<-union(bioCpackages[which(bioCpackages%in%ap)],builtins)
+
+#filter out already installed packages
+if(any(bioCpackages%in%rownames(installed.packages()))){
+  bioCpackages<-bioCpackages[-which(bioCpackages%in%rownames(installed.packages()))]
+}
+
+#number of bioconductor packages to install
+length(bioCpackages)
+
+#install packages
+BiocInstaller::biocLite(bioCpackages)
+
+#Check that installed packages are consistent with R and Bioconductor version installed
+suppressWarnings(BiocInstaller::biocValid(fix=TRUE, ask=FALSE))
+
---
+++ installers/installAxioPackages.R
@@ -0,0 +1,10 @@
+
+axioPackages<-c("AxioGWAS","AxioNanostring","AxioPC","AxioPlot","AxioQC","AxioSerializer","AxioqPCR","BGSA","BGSAsnp","svmrfe")
+
+install.packages(c("Unicode","fst","stringi","stringr","tidyverse"))
+install.packages("flock", repos="http://R-Forge.R-project.org", type="source")
+
+for(axioPackage in axioPackages){
+  print(paste("installing",axioPackage))
+  install.packages( file.path("/app/axioPackages",axioPackage) , repos = NULL , type = "source" )
+}
