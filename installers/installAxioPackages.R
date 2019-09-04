09af111d2d860228ab9003b7e0f117b15c67e4e7
Joseph Sayler <josephs@axioresearch.com>
2019-09-04 14:29:28 -0700

    Update installAxioPackages.R
    added list of prerequisites that appear to be needed to install the axio packages

--- installers/installAxioPackages.R
+++ installers/installAxioPackages.R
@@ -1,8 +1,10 @@
 
 axioPackages<-c("AxioGWAS","AxioNanostring","AxioPC","AxioPlot","AxioQC","AxioSerializer","AxioqPCR","BGSA","BGSAsnp","svmrfe")
+prerequisites<-c('haplo.stats','snpStats','car','mlogit','HH','snowfall','biomaRt','RSQLite','bit','NanoStringNorm','XML','Biobase','pwr','hexbin','XLConnect','RSQLite','blob','lz4','GSA','biomaRt','e1071','randomForest','ipred','cglNanoString','GeneticsPlot','cglQC')
 
 install.packages(c("Unicode","fst","stringi","stringr","tidyverse"))
 install.packages("flock", repos="http://R-Forge.R-project.org", type="source")
+install.packages(prerequisites)
 
 for(axioPackage in axioPackages){
   print(paste("installing",axioPackage))
